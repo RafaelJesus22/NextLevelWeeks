7cb197118dd939296c0ab7dd6c5fd32d89f687a6
RafaelJesus22 <rafael.moura22@fatec.sp.gov.br>
2020-08-05 13:50:35 -0300

    criados controllers e rota para listagem com filtro

---
+++ #2 - Proffy/server/src/controllers/classesControler.ts
@@ -0,0 +1,94 @@
+import { Request, Response } from 'express'
+
+import db from '../database/connection';
+
+
+interface ScheduleItem {
+  week_day: number;
+  from: string;
+  to: string
+}
+
+export default class ClassesControler {
+
+  async index(request: Request, response: Response) {
+    const filters = request.query;
+
+    const subject = filters.subject as string;
+    const week_day = filters.week_day as string;
+
+    
+    if (!filters.subject || !filters.week_day) {
+      return response.status(400).json({
+        error: 'Missing filters to search classes'
+      });
+    }
+
+    const classes = await db('classes')
+      .where('classes.subject', '=', subject)
+      .join('users', 'classes.user_id', '=', 'users.id')
+      .select(['classes.*', 'users.*'])
+    ;
+
+    return response.json(classes);
+
+  }
+
+
+  async create (request: Request, response: Response) {
+
+    const {
+      name,
+      avatar,
+      whatsapp,
+      bio,
+      subject,
+      cost,
+      schedule
+    } = request.body;
+  
+    const tsx = await db.transaction();
+  
+    try {
+      const insertedUsersIds =  await tsx('users').insert({
+        name,
+        avatar,
+        whatsapp,
+        bio
+      });
+    
+      const user_id = insertedUsersIds[0];
+    
+      const insertedClassesIds =  await tsx('classes').insert({
+        subject,
+        cost,
+        user_id
+      });
+    
+      const class_id = insertedClassesIds[0];
+    
+      const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+        return {
+          class_id,
+          week_day: scheduleItem.week_day,
+          from: scheduleItem.from, 
+          to: scheduleItem.to
+        };
+      })
+    
+      await tsx('class_schedule').insert(classSchedule);
+    
+      await tsx.commit();
+    
+      return response.status(201).send()
+  
+    } catch(err) {
+      await tsx.rollback();
+      
+      return response.status(400).json({
+        error: 'Unexpected error while creating new class'
+      })
+    }
+  
+  }
+}
--- #2 - Proffy/server/src/routes.ts
+++ #2 - Proffy/server/src/routes.ts
@@ -1,57 +1,12 @@
 import express from 'express';
-import db from './database/connection';
+import ClassesControler from './controllers/classesControler';
 
 const routes = express.Router();
+const classesControler = new ClassesControler();
 
-interface ScheduleItem {
-  week_day: number;
-  from: string;
-  to: string
-}
 
-routes.post('/classes', async (request, response) => {
-
-  const {
-    name,
-    avatar,
-    whatsapp,
-    bio,
-    subject,
-    cost,
-    schedule
-  } = request.body;
-
-  const insertedUsersIds =  await db('users').insert({
-    name,
-    avatar,
-    whatsapp,
-    bio
-  });
-
-  const user_id = insertedUsersIds[0];
-
-  const insertedClassesIds =  await db('classes').insert({
-    subject,
-    cost,
-    user_id
-  });
-
-  const class_id = insertedClassesIds[0];
-
-  const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-    return {
-      class_id,
-      week_day: scheduleItem.week_day,
-      from: scheduleItem.from, 
-      to: scheduleItem.to
-    };
-  })
-
-  await db('class_schedule').insert(classSchedule);
-
-  return response.send()
-
-});
+routes.post('/classes', classesControler.create);
+routes.get('/classes', classesControler.index);
 
 
 export default routes;
