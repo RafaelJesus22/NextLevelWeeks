23980d6e71f6929cb27016939e8c090321a01e02
RafaelJesus22 <rafael.moura22@fatec.sp.gov.br>
2020-08-06 14:02:53 -0300

    Componentes 'Select' e 'Textarea' criados, e altareçoes feitas na pagina 'TeacherForm'

--- #2 - Proffy/web/src/pages/TeachersForm/index.tsx
+++ #2 - Proffy/web/src/pages/TeachersForm/index.tsx
@@ -6,6 +6,8 @@ import Input from '../../components/Input';
 import warningIcon from '../../assets/images/icons/warning.svg';
 
 import './styles.css';
+import Textarea from '../../components/TextArea';
+import Select from '../../components/Select';
 
 
 function Teacherform() {
@@ -23,13 +25,59 @@ function Teacherform() {
           <Input name="name" label="Nome completo"/>
           <Input name="avatar" label="Avatar"/>
           <Input name="whatsapp" label="WhatsApp"/>
+          <Textarea name="bio" label="Biografia"/>
         </fieldset>
 
         <fieldset>
           <legend>Sobre a aula</legend>
 
-          <Input name="subject" label="Matéria"/>
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              {value: 'Artes', label: 'Artes'},
+              {value: 'Biologia', label: 'Biologia'},
+              {value: 'Ciências', label: 'Ciências'},
+              {value: 'Educação física', label: 'Educação física'},
+              {value: 'Física', label: 'Física'},
+              {value: 'Geografia', label: 'Geografia'},
+              {value: 'História', label: 'História'},
+              {value: 'Matemática', label: 'Matemática'},
+              {value: 'Português', label: 'Português'},
+              {value: 'Quimica', label: 'Quimica'}
+            ]}
+          />
+
           <Input name="Cost" label="Custo da sua hora por aula"/>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            Horarios disponíveis
+            <button type="button">
+              + Novo horário
+            </button>
+          </legend>
+
+          <div className="schedule-item">
+            <Select
+              name="week_day"
+              label="Dia da semana"
+              options={[
+                {value: '0', label: 'Domingo'},
+                {value: '1', label: 'Segunda-feira'},
+                {value: '2', label: 'Terça-feira'},
+                {value: '3', label: 'Quarta-feira'},
+                {value: '4', label: 'Quinta-feira'},
+                {value: '5', label: 'Sexta-feira'},
+                {value: '6', label: 'Sabado'}
+              ]}
+            />
+
+            <Input name="from" label="Das" type="time" />
+            <Input name="to" label="Até" type="time" />
+
+          </div>
         </fieldset>
 
         <footer>
