49541e7869947ce7aee8217aca083bfc1fb5aa3e
RafaelJesus22 <rafael.moura22@fatec.sp.gov.br>
2020-08-11 23:25:10 -0300

    front-end finalizado

--- #2 - Proffy/web/src/components/TeacherItem/index.tsx
+++ #2 - Proffy/web/src/components/TeacherItem/index.tsx
@@ -4,35 +4,44 @@ import whatsappsIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  user_id: number;
+  name: string;
+  avatar: string;
+  bio: string;
+  subject: string;
+  cost: number;
+  whatsapp: string;
+}
+
+interface teacherItemsProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<teacherItemsProps> = ({ teacher }) => {
   return(
     <article className="teacher-item">
       <header>
-        <img src="https://avatars2.githubusercontent.com/u/55329726?s=460&u=93f1b11a2037a8c10515dc8a5295b68f2ba2692b&v=4" alt="Rafael Jesus"/>
+        <img src={teacher.avatar} alt={ teacher.name }/>
         <div>
-          <strong>Rafael Jesus</strong>
-          <span>PowerApps</span>
+          <strong>{ teacher.name }</strong>
+          <span>{ teacher.subject }</span>
         </div>
       </header>
 
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-        <br/><br/>
-        Cupiditate repellendus magni recusandae dicta quis dolorem eius similique sunt aliquid temporibus.
-        Totam est placeat aspernatur eligendi facere dolorum doloremque pariatur error consequatur!
-        Excepturi ad tenetur provident eaque explicabo dicta, facilis tempore.
-      </p>
+      <p>{ teacher.bio }</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$40,00</strong>
+          <strong>R$ { teacher.cost }</strong>
         </p>
 
-        <button type="button">
+        <a href={`https://wa.me/${teacher.whatsapp}`}>
           <img src={whatsappsIcon} alt="WhatsApp"/>
           Entrar em contato
-        </button>
+        </a>
 
       </footer>
 
